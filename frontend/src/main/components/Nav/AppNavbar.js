a09c5687b94a4eeb81f73813b74a4861d3253c53
Christian Baker <christianjbaker@icloud.com>
2023-05-16 20:28:27 -0700

    cjb - another attempt to pass tests

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -77,8 +77,13 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
             <Nav className="mr-auto">
               {
                 hasRole(currentUser, "ROLE_USER") && (
-                  <NavDropdown title="Parks" id="appnavbar-parks-dropdown" data-testid="appnavbar-todos-dropdown" >
-                    <NavDropdown.Item href="/parks/create">Create Park</NavDropdown.Item>
+                  <NavDropdown title="Parks" id="appnavbar-parks-dropdown" data-testid="appnavbar-parks-dropdown" >
+                    <NavDropdown.Item href="/parks/list" data-testid="appnavbar-parks-list">List</NavDropdown.Item>
+                    {
+                      hasRole(currentUser, "ROLE_ADMIN") && (
+                        <NavDropdown.Item href="/parks/create" data-testid="appnavbar-parks-create">Create</NavDropdown.Item>
+                      )
+                    }
                   </NavDropdown>
                 )
               }
