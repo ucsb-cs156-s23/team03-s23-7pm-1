936df356ad2ff629907ae5616ee8af7dd685bdb6
Benjamin Cruttenden <cruttenden@cs.ucsb.edu>
2023-05-17 23:28:44 -0700

    bc - added mock user in index test page

--- frontend/src/tests/pages/Schools/SchoolIndexPage.test.js
+++ frontend/src/tests/pages/Schools/SchoolIndexPage.test.js
@@ -4,6 +4,12 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import mockConsole from "jest-mock-console";
 
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+
+
 const mockNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
@@ -37,6 +43,11 @@ jest.mock('main/utils/schoolUtils', () => {
 
 
 describe("SchoolIndexPage tests", () => {
+
+    // mock /api/currentUser and /api/systemInfo
+    const axiosMock =new AxiosMockAdapter(axios);
+    axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
 
     const queryClient = new QueryClient();
     test("renders without crashing", () => {
