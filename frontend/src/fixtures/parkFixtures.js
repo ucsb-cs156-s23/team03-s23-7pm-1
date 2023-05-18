31caaad07b13fdc16d418570359e4e41b80e0bbb
Christian Baker <christianjbaker@icloud.com>
2023-05-17 21:51:03 -0700

    cjb - remove extra field from parkFixtures

--- frontend/src/fixtures/parkFixtures.js
+++ frontend/src/fixtures/parkFixtures.js
@@ -5,8 +5,7 @@ const parkFixtures = {
             "id": 1,
             "name": "Yosemite National Park",
             "state": "California",
-            "acres": 761747,
-            "description": "Iconic granite cliffs, waterfalls, giant sequoias, and diverse wildlife."
+            "acres": 761747
         }
     ],
 
@@ -16,26 +15,22 @@ const parkFixtures = {
             "id": 2,
             "name": "Grand Canyon National Park",
             "state": "Arizona",
-            "acres": 1218375,
-            "description": "Spectacular landscape of deep canyons and colorful rock formations, carved over millions of years by the Colorado River."
+            "acres": 1218375
         },
 
         {
             "id": 3,
             "name": "Zion National Park",
             "state": "Utah",
-            "acres": 147242,
-            "description": "Magnificent sandstone cliffs, narrow canyons, and diverse plant and animal life."
+            "acres": 147242
         },
 
         {
             "id": 4,
             "name": "Grand Teton National Park",
             "state": "Wyoming",
-            "acres": 310044,
-            "description": "Towering mountain peaks, glacial lakes, and abundant wildlife."
-        },
-
+            "acres": 310044
+        }
     ]
 };
 
