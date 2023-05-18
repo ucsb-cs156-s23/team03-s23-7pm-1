7b82841286f80e5d30f43cfa4ef888210e9701b6
Christian Baker <christianjbaker@icloud.com>
2023-05-17 21:23:16 -0700

    cjb - add story pages

---
+++ frontend/src/stories/pages/Parks/ParksCreatePage.stories.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import ParksCreatePage from "main/pages/Parks/ParksCreatePage";
+
+export default {
+    title: 'pages/Parks/ParksCreatePage',
+    component: ParksCreatePage
+};
+
+const Template = () => <ParksCreatePage />;
+
+export const Default = Template.bind({});
+
+
+
+
---
+++ frontend/src/stories/pages/Parks/ParksEditPage.stories.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+import ParksEditPage from "main/pages/Parks/ParksEditPage";
+
+export default {
+    title: 'pages/Parks/ParksEditPage',
+    component: ParksEditPage
+};
+
+const Template = () => <ParksEditPage />;
+
+export const Default = Template.bind({});
+
+
+
